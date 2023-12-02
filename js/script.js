741be5beb879116db3a1a5a7886e441ede30ed6f
Sławomir Toporek <toporek.slawomir@gmail.com>
2023-12-02 22:05:53 +0100

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -6,15 +6,16 @@
   }); */
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
-    
+    console.log('clickedElement: ' + clickedElement);
+    clickedElement.classList.add('active');
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
     for(let activeArticle of activeArticles) {
