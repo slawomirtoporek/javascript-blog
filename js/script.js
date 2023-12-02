8f9a417ea2c2dd9a12733326ceffc0cae532be0d
Sławomir Toporek <toporek.slawomir@gmail.com>
2023-12-02 23:08:07 +0100

    add querySelector that finds the href attribute

--- js/script.js
+++ js/script.js
@@ -26,7 +26,8 @@ const titleClickHandler = function(event){
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector)
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
     /* add class 'active' to the correct article */
     
   }
